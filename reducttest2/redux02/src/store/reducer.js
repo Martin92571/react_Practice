374c6e71b3278d714f71f8934efeba2f1fbd5724
Martin92571 <zegrizo@gmail.com>
2019-03-01 22:45:50 -0800

    add reducer.js

--- reducttest2/redux02/src/store/reducer.js
+++ reducttest2/redux02/src/store/reducer.js
@@ -6,12 +6,25 @@ const intialState={
 const reducer=(state=intialState,action)=>{
    switch(action.type){
        case actionTypes.ADD_PERSON:
-
-       return{...state,}
+       
+        const newPerson = {
+            id: Math.random(), // not really unique but good enough here!
+            name: 'Max',
+            age: Math.floor( Math.random() * 40 )
+        }
+        
+       return{...state,
+        list:state.list.concat(newPerson)
+    }
 
        case actionTypes.REMOVE_PERSON:
-
-       return{...state,}
+       
+         const deletePerson= state.list.filter(person => person.id !== action.id)
+      
+    
+       return{...state,
+              list:deletePerson
+    }
    }
    return state;
 
